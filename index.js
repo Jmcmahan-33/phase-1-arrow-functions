43f4fb9db7ce7cb0d808f3dc826d63b42217c150
Jared McMahan <jaredm788@gmail.com>
2022-08-16 10:58:12 -0600

    commit tree 4bf0e9f75a8f9f89ba257d9f345864a63715a5e8

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = (parm1, parm2) => parm1 / parm2
+// divde(2000, 100)
+
+
+// first try
+// const square = (squaredNumber) => squaredNumber * squaredNumber
+// square()
+
+// second try
+// const square = (num1) => num1 * num1
+// square(num1)
+
+const square = (parm3) => parm3 * parm3
+
+const add = (parm4,parm5) => parm4 + parm5
